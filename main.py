db71d9d0f55879434c449b1c960bdcd66e92b6ab
AKSLU <152468639+AKSLU@users.noreply.github.com>
2025-09-05 20:49:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 from tkinter import *
+from tkinter import filedialog
 import os
 
 root = Tk()
 root.title("Блокнот")
-root.geometry("400x400")
-
-path_lalbel = Label(root,text="Написать название с раширением")
-path_lalbel.pack(fill=X, padx=5, pady=5)
-path_entry = Entry(root)
-path_entry.pack(fill=X, padx=10, pady=10)
+root.geometry("600x400")
 
 text = Text(root, wrap="word", undo=True)
 text.pack(expand=True, fill=BOTH)
 
-file_path = None 
+file_path = None  
 
 
 def new_file():
     global file_path
     file_path = None
-    path_entry.delete(0, END)
     text.delete(1.0, END)
     root.title("Блокнот - Новый файл")
 
 
 def open_file():
     global file_path
-    file_path = path_entry.get().strip()
-    if file_path and os.path.exists(file_path):
+    file_path = filedialog.askopenfilename(
+        defaultextension=".txt",
+        filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")]
+    )
+    if file_path:
         with open(file_path, "r", encoding="utf-8") as f:
             text.delete(1.0, END)
             text.insert(1.0, f.read())
         root.title(f"Блокнот - {os.path.basename(file_path)}")
-    else:
-        root.title("Блокнот - файл не найден")
 
 
 def save_file():
     global file_path
-    file_path = path_entry.get().strip()
+    if file_path:
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(text.get(1.0, END))
+    else:
+        save_file_as()
+
+
+def save_file_as():
+    global file_path
+    file_path = filedialog.asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")]
+    )
     if file_path:
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(text.get(1.0, END))
         root.title(f"Блокнот - {os.path.basename(file_path)}")
-    else:
-        root.title("Блокнот - укажите путь к файлу")
+
 
 menu = Menu(root)
 root.config(menu=menu)
@@ -55,5 +62,7 @@ menu.add_cascade(label="Файл", menu=file_menu)
 file_menu.add_command(label="Создать", command=new_file)
 file_menu.add_command(label="Открыть", command=open_file)
 file_menu.add_command(label="Сохранить", command=save_file)
+file_menu.add_command(label="Сохранить как", command=save_file_as)
 
 root.mainloop()
+
